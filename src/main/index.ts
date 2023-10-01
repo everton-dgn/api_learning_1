70c4a610f19616ef8072aeac47baa6fbb08dc049
Éverton Toffanetto <evertondgn@hotmail.com>
2023-10-01 20:52:22 -0300

    fix: fix config

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,6 @@ import app from './app'
 
 const port = Number(process.env.APP_PORT)
 
-app.listen({ port, host: '0.0.0.0' }, err => {
+app.listen({ port }, err => {
   if (err) app.log.error(err)
 })
