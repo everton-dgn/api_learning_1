a4c393e0a8007ac748e48cbe4c6933d89e9d6120
Éverton Toffanetto <evertondgn@hotmail.com>
2023-10-02 01:02:43 -0300

    test: add tests

--- src/tests/integration/app.spec.ts
+++ src/tests/integration/app.spec.ts
@@ -1,15 +1,38 @@
 import request from 'tests/setup.js'
 
 describe('Testando as rotas', () => {
-  it('Deve responder com "Hello World!" em /', async () => {
-    const response = await request.get('/')
+  it('Deve retornar uma lista de vídeos nas chamadas GET para rota /videos', async () => {
+    const response = await request.get('/videos')
     expect(response.status).toBe(200)
-    expect(response.body.message).toBe('Hello World!')
+    expect(response.body.length).toBe(7)
   })
 
-  it('Deve responder com "abc" em /abc', async () => {
-    const response = await request.get('/abc')
-    expect(response.status).toBe(200)
-    expect(response.body.message).toBe('abc')
+  it('should list videos', async () => {
+    const response = await request.get('/videos').query({ search: 'Test' })
+    expect(response.status).to.equal(200)
+    expect(response.body.length).toBe(3)
+  })
+
+  it.skip('should create a new video', async () => {
+    const response = await request.post('/videos').send({
+      title: 'Test Video',
+      description: 'Test Description',
+      duration: 120
+    })
+    expect(response.status).toBe(201)
+  })
+
+  it.skip('should update a video', async () => {
+    const response = await request.put('/videos/1').send({
+      title: 'Updated Video',
+      description: 'Updated Description',
+      duration: 130
+    })
+    expect(response.status).toBe(204)
+  })
+
+  it.skip('should delete a video', async () => {
+    const response = await request.delete('/videos/1')
+    expect(response.status).toBe(204)
   })
 })
