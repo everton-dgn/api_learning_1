5dc5a15c028500a427c701e61c607044297f0acd
Éverton Toffanetto <evertondgn@hotmail.com>
2023-10-05 03:33:39 -0300

    fix: fix types and validations

--- src/domain/services/video-service.ts
+++ src/domain/services/video-service.ts
@@ -1,34 +1,38 @@
 import type { VideoAggregate, VideoModel } from 'domain/models/video-contracts'
 import type { UUID } from 'node:crypto'
 import type { VideoRepository } from 'domain/repositories/video-repository'
+import z from 'zod'
+
+export const UUIDSchema = z.string().uuid()
+export const VideoModelSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  duration: z.number().positive()
+})
 
 export interface VideoService {
   create: (video: VideoModel) => Promise<VideoAggregate>
-  find: (
-    criteria: Record<string, string>
-  ) => Promise<VideoAggregate | undefined>
+  find: (criteria: Record<string, string>) => Promise<VideoAggregate[]>
   list: () => Promise<VideoAggregate[]>
   remove: (id: UUID) => Promise<boolean>
   update: (video: VideoModel, id: UUID) => Promise<boolean>
 }
 
-export abstract class VideoServiceImplementation implements VideoService {
-  protected constructor(private readonly repository: VideoRepository) {}
+export class VideoServiceImplementation implements VideoService {
+  constructor(private readonly repository: VideoRepository) {}
 
   async create(video: VideoModel): Promise<VideoAggregate> {
     return this.repository.create(video)
   }
 
-  async find(
-    criteria: Record<string, string>
-  ): Promise<VideoAggregate | undefined> {
+  async find(criteria: Record<string, string>): Promise<VideoAggregate[]> {
     const result = await this.repository.find(criteria)
-    if (result.length === 0) throw new Error('Video not found')
-    return result.pop()
+    if (!result.length) throw new Error('Video not found!')
+    return result
   }
 
   async list(): Promise<VideoAggregate[]> {
-    return this.repository.find({})
+    return this.repository.find({ search: '' })
   }
 
   async remove(id: UUID): Promise<boolean> {
@@ -36,6 +40,16 @@ export abstract class VideoServiceImplementation implements VideoService {
   }
 
   async update(video: VideoModel, id: UUID): Promise<boolean> {
-    return this.repository.update(id, video)
+    const idValidationResult = UUIDSchema.safeParse(id)
+    if (!idValidationResult.success) {
+      throw new Error('Invalid id')
+    }
+
+    const videoValidationResult = VideoModelSchema.safeParse(video)
+    if (!videoValidationResult.success) {
+      throw new Error('Invalid video')
+    }
+
+    return this.repository.update(video, id)
   }
 }
